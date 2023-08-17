ce04d08e76dab19668e83d21fbb9a967fb158077
Jackson-O-J <81090159+Jackson-O-J@users.noreply.github.com>
2023-08-17 23:24:32 +0530

    Update streamlit_app.py
    Write Code to Add Rows to Our Fruit List in Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+#Challenge accepted to add second enry box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#thi will not work correctly
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
